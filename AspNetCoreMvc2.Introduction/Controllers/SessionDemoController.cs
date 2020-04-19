2ff45ca61d50290a2b70be866db9e240b3d0993a
Umut Yılmaz <u.ylmz.96@gmail.com>
2020-04-19 17:05:06 +0300

    Session
    Session nasıl kullanılır ve ExtensionMethod nasıl yazılır.

---
+++ AspNetCoreMvc2.Introduction/Controllers/SessionDemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AspNetCoreMvc2.Introduction.ExtensionMethods;
+using AspNetCoreMvc2.Introduction.DataSources.Entities;
+
+namespace AspNetCoreMvc2.Introduction.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        public JsonResult Index()
+        {
+            HttpContext.Session.SetInt32("age", 23);
+            HttpContext.Session.SetString("name", "Umut");
+            //Extension Methot
+            HttpContext.Session.SetObject("param", new Parameter { Key = "Test", Value = "Test", Description = "Test" });
+            return Json("Session Created");
+        }
+
+        public string GetSession()
+        {
+
+            return String.Format("Hello {0},You are {1} Parameter is {2}"
+                                , HttpContext.Session.GetString("name")
+                                , HttpContext.Session.GetInt32("age")
+                                , HttpContext.Session.GetObject<Parameter>("param").Key); //ExtensionMethod
+        }
+    }
+}
