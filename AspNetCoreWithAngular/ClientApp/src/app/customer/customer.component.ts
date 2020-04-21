efd0d94e9535978d160a6e86583049da0e9aece1
Umut Yılmaz <u.ylmz.96@gmail.com>
2020-04-21 15:07:51 +0300

    Angular Örnek

---
+++ AspNetCoreWithAngular/ClientApp/src/app/customer/customer.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'customer',
+  templateUrl: './customer.component.html'
+})
+export class CustomerComponent {
+  public customers: Customer[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Customer[]>(baseUrl + 'api/customers').subscribe(result => {
+      this.customers = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Customer {
+  id: number;
+  firstName: number;
+  lastName: number;
+}
